272c46bb5abf50a1af6e9877356bfc93d4e2ba64
Michael <michael.r808@gmail.com>
2015-07-12 12:30:45 -0500

    Initial Commit

---
+++ ValleyForge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ValleyForge
+//
+//  Created by mike on 7/12/15.
+//  Copyright (c) 2015 X-Tic Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ValleyForge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ValleyForge
+//
+//  Created by mike on 7/12/15.
+//  Copyright (c) 2015 X-Tic Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
