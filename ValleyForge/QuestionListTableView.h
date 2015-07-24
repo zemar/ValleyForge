a0a6f67725183bc239b53f2fd8e231d2f90e3cdd
Michael <michael.r808@gmail.com>
2015-07-23 22:30:19 -0500

    commit

---
+++ ValleyForge/QuestionListTableView.h
@@ -0,0 +1,13 @@
+//
+//  QuestionListTableView.h
+//  ValleyForge
+//
+//  Created by mike on 7/12/15.
+//  Copyright (c) 2015 X-Tic Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionListTableView : UITableView
+
+@end
