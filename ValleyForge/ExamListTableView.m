cdc55cbbb739374cb445ab044351ed56d3d6f3b5
Michael <michael.r808@gmail.com>
2015-08-16 15:56:02 -0500

    commit

---
+++ ValleyForge/ExamListTableView.m
@@ -0,0 +1,21 @@
+//
+//  ExamListTableView.m
+//  ValleyForge
+//
+//  Created by mike on 8/16/15.
+//  Copyright (c) 2015 X-Tic Systems. All rights reserved.
+//
+
+#import "ExamListTableView.h"
+
+@implementation ExamListTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ ValleyForge/ExamListTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ExamListTableViewCell.m
+//  ValleyForge
+//
+//  Created by mike on 8/16/15.
+//  Copyright (c) 2015 X-Tic Systems. All rights reserved.
+//
+
+#import "ExamListTableViewCell.h"
+
+@implementation ExamListTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
